ce666d33b09780142103bb2e4e2d9c8c05b6f50d
leonardo <leonardocasa790@gmail.com>
2023-12-12 17:30:11 -0300

    chore: initial structure class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 
 
 export class Rune{
-   private input: Array<string>;
    private rules: Array<object>;
 
    constructor(input: Array<string>, rules: Array<object>){
-      
+      this.rules = rules;
    }
+
+   process(input: Array<string>): void{
+
+
+   }
+
 }
