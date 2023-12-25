5d93c07bfdaddc9cbd3f28036566dfd67e46e827
leonardo <leonardocasa790@gmail.com>
2023-12-25 20:20:54 -0300

    chore(project): refactor entire codebase

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,7 @@
-import { rule } from "../types/types.d";
 
 export class Rune{
-   private rules: rule[];
 
-   constructor(rules: rule[]){
-      this.rules = rules
-   }
-
-   process(input: string | string[]): void{
-      let index: number = 0;
-      
-      if(Array.isArray(input)){
-         for(const rule of this.rules){
-            this.apply(input, rule)
-         }
-      }
-      else{
-         for(const rule of this.rules){
-             if(rule.pattern.test(input)){
-               if(rule.action){
-                  rule.action()
-               }
-             }
-         }
-      }
-
-   }
-
-   private apply(input: string[], rules: rule): void{
-      for(const i of input){
-         if(rules.pattern.test(i)){
-            if(rules.action){
-               rules.action()
-            }
-         }
-      }
-   }
 
 };
 
-export { rule };
 
-
-
-
-
-      // if(Array.isArray(this.rules)){
-      //    for(const rule of this.rules){
-      //       this.apply(input, rule)
-      //    }
-      // }
-   // }
-   // private apply(input: string[], rules: rule): void{
-   //    for(const i of input){
-   //       if(rules.pattern.test(i)){
-   //          if(rules.action){
-   //             rules.action();
-   //          }
-   //       };
-   //    };
-   // };
