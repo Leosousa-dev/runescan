1fd16489e0b9c4ac5379116998559ec9698d9a53
leonardo <leonardocasa790@gmail.com>
2023-12-25 20:32:32 -0300

    chore(class rune): create initial structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 
 export class Rune{
+   private value: number | string;
+   private cases: Map<number | string, () => void> = new Map();
+   private default: (() => {}) | null = null;
+
+   constructor(value: string | number){
+     this.value = value;
+   };
+
+ 
 
 
 };
