9e31d747907ebc4e02c0cbcb89f6242778c358c5
leonardo <leonardocasa790@gmail.com>
2024-01-05 01:03:52 -0300

    chore(index): import methods

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
+import match from "./core/methods/match";
 
-// const match = (value: any) => ({
-//    with: (expected: any, fn: () => void) => {
-//       if(expected === value) fn();
-//       return match(value);
-//    },
-//    default: (fn: () => void) => fn(),
-//  });
-
-
-//  const test = match("dd")
-//  .with("dd", ()=> console.log("dd"))
-//  .with("dd", ()=> console.log("ok leo"))
-//  .default(() => console.log("defaul here"))
