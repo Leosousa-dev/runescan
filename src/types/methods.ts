afdf6f8948b077424ec69ad248ed41200ef58a7e
leonardo <leonardocasa790@gmail.com>
2024-01-08 20:18:51 -0300

    chore(types): refact types

--- src/types/methods.ts
+++ src/types/methods.ts
@@ -1,8 +1,17 @@
-type Fn = () => void
 
-type Matche<T> = (expected: T, fn: Fn) => MatcheResu<T>;
+export type MatcherFunction = (expected: any, fn: () => void) => MatcherResult;
 
-interface MatcheResu<T> {
-   with: Matche<T>;
-   default: (fn: Fn) => void;
+export interface MatcherResult {
+  with: MatcherFunction;
+  default: (fn: () => void) => void;
 }
+
+export type Match = (value: any) => MatcherResult;
+
+export type MatchedContext = {
+  matched: boolean;
+  value?: any;
+};
+
+
+export type voidFn = () => void
