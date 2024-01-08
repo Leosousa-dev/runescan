2b518aa96692a9ad7389ac52c3a1bff020b5945c
leonardo <leonardocasa790@gmail.com>
2024-01-08 20:21:37 -0300

    chore(export): refact index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import match from "./core/methods/match/match";
+import {match} from "./core/methods/match";
 
 export {match};
-
-
-match("leo")
-.with("leo1", ()=> console.log("ok1"))
-.with("leo2", ()=> console.log("ok2"))
-.with("leo", ()=> console.log("ok correct"))
-.default(()=> console.log("default fn")) 
