f97540c1b504df6aa28cdd80420d3c1da9ed88b9
leonardo <leonardocasa790@gmail.com>
2024-01-08 20:20:56 -0300

    chore(methods): refact method with

---
+++ src/core/methods/with.ts
@@ -0,0 +1,12 @@
+import {MatchedContext, MatcherResult, voidFn} from "../../types/methods"
+import {_default} from "./default"
+
+const _with = (context: MatchedContext, expected: any, fn: voidFn ): MatcherResult => {
+   if(expected === context.value){
+      fn()
+      context.matched = true;
+   }
+   return {with: _with.bind(null, context), default: _default.bind(null, context)};
+}
+
+export {_with}
