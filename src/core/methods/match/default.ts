c45528e90f3d912dd3173ea8fabce8d0eba40864
leonardo <leonardocasa790@gmail.com>
2024-01-07 22:51:48 -0300

    fix(methods): call default once

--- src/core/methods/match/default.ts
+++ src/core/methods/match/default.ts
@@ -1,5 +1,10 @@
-const _default = (fn: ()=> void) => {
-   fn();
+const _default = (fn: () => void, matched: boolean) => {
+
+   if(matched){
+      console.log("matched true");
+      fn()
+   }
+   return;
 }
 
 export default _default;
