9fe5f4c12ae3b04276b1ef9c0d846b48d9f69da2
leonardo <leonardocasa790@gmail.com>
2024-01-17 23:25:01 -0300

    chore(methods): refact names methods

--- src/core/methods/match.ts
+++ src/core/methods/match.ts
@@ -1,10 +1,10 @@
 import {_with} from "./with";
-import {_default} from "./default";
+import {outherWise} from "./default";
 import {Match, MatchedContext, MatcherResult} from "../../types/methods"
 
 const match: Match = (value: any): MatcherResult => {
-   const context: MatchedContext = {matched: false, value};
-   return { with: _with.bind(null, context), default: _default.bind(null, context)};
+   const context: MatchedContext = {hasMatched: false, value};
+   return { with: _with.bind(null, context), outherWise: outherWise.bind(null, context)};
 };
 
 export {match}
