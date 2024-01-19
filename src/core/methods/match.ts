56b0f0b4f94effe1e95043612bf6e01c7314cccc
leonardo <leonardocasa790@gmail.com>
2024-01-18 23:13:59 -0300

    chore(methods): update indentation

--- src/core/methods/match.ts
+++ src/core/methods/match.ts
@@ -1,10 +1,14 @@
 import {_with} from "./with";
 import {outherwise} from "./outherwise";
-import {Match, MatchedContext, MatcherResult} from "../../types/methods"
+import {Match, MatchedContext, MatcherResult} from "../../types/methods";
 
 const match: Match = (value: any): MatcherResult => {
    const context: MatchedContext = {hasMatched: false, value};
-   return { with: _with.bind(null, context), outherWise: outherwise.bind(null, context)};
+
+   return { 
+      with: _with.bind(null, context),
+      outherWise: outherwise.bind(null, context)
+   };
 };
 
-export {match}
+export {match};
