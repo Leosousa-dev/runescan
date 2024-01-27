cdef8bc4337c3225465fc141214907e9806d39a5
leonardo <leonardocasa790@gmail.com>
2024-01-27 19:43:41 -0300

    chore(methods): refact match

--- src/core/match.ts
+++ src/core/match.ts
@@ -1,14 +1,10 @@
 import {_with} from "./with";
 import {outherwise} from "./outherwise";
-import {Match, MatchedContext, MatcherResult} from "../types/methods";
+import {Match, MatcherResult} from "../types/methods";
 
-const match: Match = (value: any): MatcherResult => {
-   const context: MatchedContext = {hasMatched: false, value};
+const match: Match = (value: any): MatcherResult => ({
+   with: _with (value),
+   otherWise: outherwise,
+})
 
-   return { 
-      with: _with.bind(null, context),
-      outherWise: outherwise.bind(null, context),
-   };
-};
-
-export {match};
+export default match;
