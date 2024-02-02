9768e0e94894fee4f229be4c196dc24d42786179
leonardo <leonardocasa790@gmail.com>
2024-02-02 00:09:30 -0300

    chore(methods) implement  withfn

--- src/core/match.ts
+++ src/core/match.ts
@@ -1,6 +1,10 @@
-
+import withfn from "./with";
 
 const match = (value: any) => ({
-   with: (pattern: any, callback: any) => {
+   with: (pattern: any, callback: ()=> void) => {
+      const result = withfn(value, pattern, callback)
+      return result !== undefined ? match(result) : match(value)
    }
-})
+});
+
+export default match;
