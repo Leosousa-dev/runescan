dedca736e201b7cb7747606b47d3eeebb60a76fc
leonardo <leonardocasa790@gmail.com>
2024-02-02 00:11:01 -0300

    chore(methods) logic method  withfn

--- src/core/with.ts
+++ src/core/with.ts
@@ -1,6 +1,9 @@
 
 const withfn = (value: any, pattern: any, callback: ()=> void) => {
-
-}
+      if(value === pattern){
+         return true;
+      }
+   return false;
+};
 
 export default withfn;
