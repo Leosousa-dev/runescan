7744ad334d3a8286978a3536e1d37431feb070dc
leonard <leonardocasa790@gmail.com>
2024-02-10 00:20:41 -0300

    chore: remove log

--- src/core/otherwise.ts
+++ src/core/otherwise.ts
@@ -1,7 +1,6 @@
 import { matchObjType, voidCallback } from "../types/index"
 
 const otherwise = (matchObj: matchObjType, callback: voidCallback) => {
-    console.log("verifc macthed",matchObj.matched)
     if(!matchObj.matched){
         callback()
     }
